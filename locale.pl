b90473867a0f2fab8b4adabd29235c79d8158b06
lingyingyang <41569566+lingyingyang@users.noreply.github.com>
2019-05-18 08:53:37 +1000

    Initial Project

---
+++ locale.pl
@@ -0,0 +1,95 @@
+﻿#!/usr/bin/perl -w
+
+$opt = $ARGV[0];
+
+if($opt eq "-a"){
+        #Available locales
+        scanFile("locale");
+        printResult("locale");
+        close(INFILE);
+}elsif($opt eq "-m"){
+        #Available charmaps
+        scanFile("charmap");
+        printResult("charmap");
+        close(INFILE);
+}elsif($opt eq "-s"){
+        #Total size in bytes of all locales: <total size in bytes of all locales>
+        scanFile("locale");
+        if(scalar(@sizeAry) > 0){
+                $sizeSum = 0;
+                foreach $size(@sizeAry){
+                        $sizeSum += $size;
+                }
+                print "Total size in bytes of all locales: " . $sizeSum . "\n";
+        }else{
+                print "Total size in bytes of all locales: 0\n";
+        }
+        close(INFILE);
+}elsif($opt eq "-l"){
+        #Language <name>:
+        #Total number of locales: <total number of locales in that language (possibly 0)>
+        #Total number of charmaps: <total number of charmaps in that language (possibly 0)>
+        $lang = $ARGV[1];
+        scanFile("locale");
+        $langLocalesNum = calculate($lang);
+        #print "$langLocalesNum\n";
+
+        scanFile("charmap");
+        $langCharmapsNum = calculate($lang);
+        #print "$langCharmapsNum\n";
+
+        if($langLocalesNum == 0 && $langCharmapsNum == 0){
+                print "No locales or charmaps in this language\n";
+        }else{
+                print "Total number of locales: $langLocalesNum\n";
+                print "Total number of charmaps: $langCharmapsNum\n";
+        }
+	close(INFILE);
+}elsif($opt eq "-v"){
+        print "Student Name: Lingying Yang, Student ID: 12776136\n";
+}else{
+        print "Invalid command syntax\n";
+}
+
+sub scanFile{
+        @typeAry = ();
+        @languageAry = ();
+        @filenameAry = ();
+        @sizeAry = ();
+        $file = $ARGV[$#ARGV];
+
+        #verify the input file.
+        (-r $file) or die "Filepath not found!";
+        (-f $file) or die "It is not a file!";
+        (-r $file) or die "The file is not readable!";
+
+        open(INFILE,$file) or die "Error message: $!";
+
+        $i = 0;
+        while(<INFILE>){
+                if(/^$_[0]/){
+                        ($typeAry[$i],$languageAry[$i],$filenameAry[$i],$sizeAry[$i]) = split(/,/);
+                        $i++;
+                }
+        }
+}
+
+sub printResult{
+        if(scalar(@filenameAry) > 0){
+                foreach $filename(@filenameAry){
+                        print "$filename\n";
+                }
+        }else{
+                print "No $_[0]s available\n";
+        }
+}
+
+sub calculate{
+        my $total = 0;
+        foreach $language(@languageAry){
+                if($language =~ m/$_[0]/){
+                        $total++;
+                }
+        }
+        return $total;
+}
---
+++ partitionbrowser.pl
@@ -0,0 +1,125 @@
+﻿#!/usr/bin/perl -w
+use strict;
+
+#The length of ARGV
+my $argvLength = $#ARGV+1;
+
+if($argvLength==0){
+	print "Please enter right format !!!\n";
+}elsif($argvLength==2 && $ARGV[0] eq "-n"){
+	numberPartitions($ARGV[1]);
+}elsif($argvLength==2 && $ARGV[0] eq "-u"){
+	usedSpacePartitions($ARGV[1]);
+}elsif($argvLength==2 && $ARGV[0] eq "-f"){
+	freeSpacePartitons($ARGV[1]);
+}elsif($argvLength==2 && $ARGV[0] eq "-s"){
+	studentInfo($ARGV[1]);
+}elsif($argvLength==3 && $ARGV[0] eq "-t" ){
+	mountPointPartitions($ARGV[1],$ARGV[2]);
+}else{
+	print "Please enter right format !!!\n";
+}
+
+#option is "-n";
+sub numberPartitions{
+	my $file = $_[0];
+
+	#verify the input file.
+	(-r $file) or die "Filepath not found!";
+	(-f $file) or die "It is not a file!";
+	(-r $file) or die "The file is not readable!";
+	open(INFILE,$file) or die "Error message: $!";
+
+    my $count = 0;
+    while (<INFILE>){
+		chomp;
+		#Skip blank line
+		if($_ ne ""){
+			$count++;
+		}
+		if($count == 0){
+			print "No partitions found\n";
+		}else{
+			print "Number of partitions: $count\n";
+		}
+    }
+}
+
+#option is "-u";
+sub usedSpacePartitions{
+	my $file = $_[0];
+
+	#verify the input file.
+	(-r $file) or die "Filepath not found!";
+	(-f $file) or die "It is not a file!";
+	(-r $file) or die "The file is not readable!";
+	open(INFILE,$file) or die "Error message: $!";
+	
+    my $usedSpace = 0;
+    while (<INFILE>){
+      chomp;
+      if($_ ne ""){
+        my @partitions = split(/ /,$_);
+        $usedSpace+=$partitions[3];
+      }
+    }
+    print "Used space: $usedSpace\n";
+}
+
+#option is "-f";
+sub freeSpacePartitons{
+	my $file = $_[0];
+
+	#verify the input file.
+	(-r $file) or die "Filepath not found!";
+	(-f $file) or die "It is not a file!";
+	(-r $file) or die "The file is not readable!";
+	open(INFILE,$file) or die "Error message: $!";
+	
+	my $freeSpace=0;
+	while (<INFILE>){
+		chomp;
+		if($_ ne ""){
+		  my @partitions = split(/ /,$_);
+		  $freeSpace+=$partitions[2]-$partitions[3];
+		}
+	}
+    print "Free space: $freeSpace\n";
+}
+
+#option is "-t";
+sub mountPointPartitions{
+    my $file = $_[1];
+
+	#verify the input file.
+	(-r $file) or die "Filepath not found!";
+	(-f $file) or die "It is not a file!";
+	(-r $file) or die "The file is not readable!";
+	open(INFILE,$file) or die "Error message: $!";
+	
+    my $fileSysType=$_[0];
+    my $equalCount=0;
+    while(<INFILE>){
+		chomp;
+		if($_ ne ""){
+			my @partitions = split(/ /,$_);
+			if($partitions[1] eq $fileSysType){
+			  if($equalCount==0){
+				print "Mount points with filesystem type $fileSysType:\n";
+			  }
+			  print $partitions[0]."\n";
+			  $equalCount++;
+			}
+		}
+    }
+    if ($equalCount==0) {
+		print "No mount points with this filesystem type\n";
+    }
+}
+
+#option is "-s";
+sub studentInfo{
+	print "   Student name : "."Qian Ding"."\n";
+	print "     Student ID : "."13101023"."\n";
+	print "           Date : "."16/10/2018"."\n";
+}
